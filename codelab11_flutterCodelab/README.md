18e6124e9cfbe74d75e9ff04d8f9aac3ae60fcdd
Fatikah002 <sfatikah2005@gmail.com>
2025-11-06 13:33:04 +0700

    W11: Soal 2

---
+++ codelab11_flutterCodelab/README.md
@@ -0,0 +1,17 @@
+
+### Soal 1
+* Tambahkan nama panggilan Anda pada **title** app sebagai identitas hasil pekerjaan Anda.
+```dart
+return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future Fatikah'),
+      ),
+```
+
+### Soal 2
+* Carilah judul buku favorit Anda di Google Books, lalu ganti ID buku pada variabel path di kode tersebut. Caranya ambil di URL browser Anda seperti gambar berikut ini.
+![alt text](img/image.png)
+
+* Kemudian cobalah akses di browser URI tersebut dengan lengkap seperti ini. Jika menampilkan data JSON, maka Anda telah berhasil. Lakukan capture milik Anda dan tulis di README pada laporan praktikum. Lalu lakukan commit dengan pesan "W11: Soal 2".
+![alt text](img/image1.png)
+    - ![alt text](img/image2.png)
---
+++ codelab11_flutterCodelab/books/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<http.Response> getData() async {
+  const authority = 'www.googleapis.com';
+  const path = '/books/v1/volumes/WwPHEAAAQBAJ';
+  final url = Uri.https(authority, path);
+  return await http.get(url);
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future Fatikah'),
+      ),
+      body: Center(
+        child: Column(children: [
+          const Spacer(),
+          ElevatedButton(
+            child: const Text('GO!'),
+            onPressed: () {},
+          ),
+          const Spacer(),
+          Text(result),
+          const Spacer(),
+          const CircularProgressIndicator(),
+          const Spacer(),
+        ]),
+      ),
+    );
+  }
+}
