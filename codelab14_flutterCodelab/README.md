210099a995de57b2087259d01c068bbc3388972c
Fatikah002 <sfatikah2005@gmail.com>
2025-12-01 16:33:04 +0700

    W14: Jawaban Soal 1

---
+++ codelab14_flutterCodelab/README.md
@@ -0,0 +1,5 @@
+### Soal 1
+![alt text](img/S1.png)
+### Soal 2
+### Soal 3
+### Soal 4
---
+++ codelab14_flutterCodelab/restful_api/lib/model/pizza.dart
@@ -0,0 +1,30 @@
+const String keyId = 'id';
+const String keyName = 'pizzaName';
+const String keyDescription = 'description';
+const String keyPrice = 'price';
+const String keyImage = 'imageUrl';
+
+class Pizza {
+	final int id;
+	final String pizzaName;
+	final String description;
+	final double price;
+	final String imageUrl;
+
+	Pizza.fromJson(Map<String, dynamic> json)
+			: id = int.tryParse(json[keyId].toString()) ?? 0,
+				pizzaName = json[keyName] != null ? json[keyName].toString() : 'No name',
+				description = json[keyDescription] != null ? json[keyDescription].toString() : '',
+				price = double.tryParse(json[keyPrice].toString()) ?? 0,
+				imageUrl = json[keyImage]?.toString() ?? '';
+
+	Map<String, dynamic> toJson() {
+		return {
+			keyId: id,
+			keyName: pizzaName,
+			keyDescription: description,
+			keyPrice: price,
+			keyImage: imageUrl,
+		};
+	}
+}
